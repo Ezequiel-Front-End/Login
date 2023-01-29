d027ffc98e22b005de29bc307adb09cd1bed3557
Ezequiel Fernandes <ezequielfernandes912@gmail.com>
2023-01-29 11:35:01 -0300

    Sucesso...

---
+++ index.js
@@ -0,0 +1,18 @@
+// comprimento para quando vizualizarem a pagina 
+// comando alert em JavaScript
+alert("Seja bem vindo!(a)");
+
+// navegação entre telas
+// faceboock
+function Navegar(){
+    window.location.href = "https://www.facebook.com/";
+} 
+// intragram
+function Navegar_One(){
+    window.location.href = "https://www.instagram.com/";
+} 
+
+// email
+function Navegar_Two(){
+    window.location.href = "https://www.google.com/intl/pt-BR/gmail/about/";
+} 
